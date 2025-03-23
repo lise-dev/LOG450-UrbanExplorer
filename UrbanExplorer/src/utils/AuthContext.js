79a91190ddd36a45d20f88127d7e721ba3bd980d
loceny6 <loceny.soumaoro.1@ens.etsmtl.ca>
2025-03-23 11:40:54 -0400

    ajout de userContext pour controler la session de l'utilisateur et utiliser ses informations

---
+++ UrbanExplorer/src/utils/AuthContext.js
@@ -0,0 +1,25 @@
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {onAuthStateChanged} from 'firebase/auth';
+import {auth} from 'firebaseConfig';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [initializing, setInitializing] = useState(true);
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser);
+            setInitializing(false);
+        });
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, userId: user?.uid }}>
+            {!initializing && children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
