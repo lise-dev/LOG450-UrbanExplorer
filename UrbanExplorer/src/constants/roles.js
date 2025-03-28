f1efc76f122f800454baca44cd255e3ff1b23494
Simon Villard <simon.vllrd@gmail.com>
2025-03-28 17:39:42 -0400

    fixed picker register + const roles

---
+++ UrbanExplorer/src/constants/roles.js
@@ -0,0 +1,12 @@
+// export default Roles = {
+//     EXPLORATEUR: 'explorateur',
+//     CONTRIBUTEUR: "contributeur",
+//     MODERATEUR: "moderateur"
+// };
+
+
+export default Roles = [
+    "explorateur",
+    "contributeur",
+    "moderateur"
+];
