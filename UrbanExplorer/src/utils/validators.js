b72b033033b58bc3e0f81ce949fc49b45c72fb6f
Simon Villard <simon.vllrd@gmail.com>
2025-04-07 22:51:40 -0400

    bug validator role

--- UrbanExplorer/src/utils/validators.js
+++ UrbanExplorer/src/utils/validators.js
@@ -25,7 +25,7 @@ export const checkEmailExists = async (email) => {
 
 // Vérifier si un rôle est valide
 export const isValidRole = (role) => {
-  return roles.includes(role);
+  return Object.values(roles).includes(role);
 };
 
 // Vérifier si une note est valide (entre 1 et 5)
